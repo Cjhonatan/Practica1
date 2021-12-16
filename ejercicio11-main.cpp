9a065bc784817ee809a96afa467be6b96341052b
Cjhonatan <96266315+Cjhonatan@users.noreply.github.com>
2021-12-16 17:18:01 -0500

    cpp ejercicio11-P1

---
+++ ejercicio11-main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a, b, c=0;
+    cout << "Ingrese el un numero: " << endl;
+    cin >> a;
+
+    for (b=1; b<=10; b++){
+
+    c=a*b;
+
+    cout <<b<< " x "<<a<< " = "<<c<<endl;
+
+
+
+
+
+    }
+
+return 0;
+}
