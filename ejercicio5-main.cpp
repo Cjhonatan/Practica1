75e0243280570c73cd48c3eba0b250b2cac72406
Cjhonatan <96266315+Cjhonatan@users.noreply.github.com>
2021-12-16 16:41:01 -0500

    cpp ejercicio5-P1

---
+++ ejercicio5-main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cout<<"Ingresa A: ";
+    cin>>A;
+    int B;
+    cout<<"Ingresa B: ";
+    cin>>B;
+int C;
+float D;
+float E;
+
+C=A/B;
+D=A/B;
+E=D-C;
+
+    if (E<0.5){
+        cout<<A<<"/"<<B<<" = "<<C+1<<endl;
+    }
+    else {
+        cout<<A<<"/"<<B<<" = "<<C<<endl;
+    }
+
+    return 0;
+}
