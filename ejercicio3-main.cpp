93bea85c66f56ab76b05399965aec2a8763653f4
Cjhonatan <96266315+Cjhonatan@users.noreply.github.com>
2021-12-16 16:40:11 -0500

    cpp ejercicio3-P1

---
+++ ejercicio3-main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A, B;
+
+    cout << "Ingrese un numero: " <<endl;
+    cin >> A;
+    cout << "Ingrese otro numero " <<endl;
+    cin >> B;
+
+
+    if (A>B){
+
+        cout << "El mayor es " <<A<< endl;
+
+    }
+
+        else {
+
+           cout << "El mayor es " <<B<< endl;
+        }
+
+
+
+}
