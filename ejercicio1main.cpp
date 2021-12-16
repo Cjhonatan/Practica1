85f9290ce2e39dc5e4e92ecda5a28798913a047f
Cjhonatan <96266315+Cjhonatan@users.noreply.github.com>
2021-12-16 16:31:00 -0500

    cpp ejercicio1-P1

---
+++ ejercicio1main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A , B;
+
+    cout << "Ingrese el numero A: " << endl;
+    cin >>A;
+    cout << "Ingrese el numero B: " << endl;
+    cin >>B;
+
+    cout << "El residuo de la division " <<A<< '/' <<B<< " es: " << A%B<<endl;
+
+    return 0;
+
+
+
+
+
+}
