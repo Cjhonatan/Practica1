46d1d4647af7533c3bd9971d112aa0ce16d28442
Cjhonatan <96266315+Cjhonatan@users.noreply.github.com>
2021-12-16 17:06:48 -0500

    cpp ejercicio9-P1

---
+++ ejercicio9-main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int r;
+
+   cout<<"Ingrese el Radio del circulo: "<<endl;
+   cin >>r;
+
+   float p = 2*3.1416*r;
+   float a = 3.1416*r*r;
+
+   cout <<"Perimetro: "<< p<<endl;
+   cout <<"Area: "<<a<<endl;
+
+
+   return 0;
+
+
+
+
+
+
+
+
+
+
+}
